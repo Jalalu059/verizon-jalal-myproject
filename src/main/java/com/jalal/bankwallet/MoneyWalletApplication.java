5791cc5785b537d33c5926cc298f93eef437b92a
Jalalu059 <mdjalal56059@gmail.com>
2024-02-15 12:51:55 +0530

    adding moneybank src code

---
+++ src/main/java/com/jalal/bankwallet/MoneyWalletApplication.java
@@ -0,0 +1,13 @@
+package com.jalal.bankwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jalal/bankwallet/MoneyWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalal.bankwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
